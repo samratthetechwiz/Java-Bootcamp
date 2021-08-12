bb3e1df361bd678c3df4ab97cd81610192d9347a
Samrat Dutta <samratthetechwiz@gmail.com>
2021-08-13 00:11:29 +0530

    Largest of Two Numbers and Greetings Program

---
+++ src/com/samrat/LargestOfTwo.java
@@ -0,0 +1,18 @@
+package com.samrat;
+
+import java.util.Scanner;
+
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter 2 numbers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        if(num1 > num2){
+            System.out.println(num1 + " is greater");
+        }else{
+            System.out.println(num2 + " is greater");
+        }
+    }
+}
