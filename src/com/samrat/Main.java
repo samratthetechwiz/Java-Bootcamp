16844b40210466a90348268fddd6344366bbf411
Samrat Dutta <samratthetechwiz@gmail.com>
2021-08-12 23:54:17 +0530

    First commit

---
+++ src/com/samrat/Main.java
@@ -0,0 +1,11 @@
+package com.samrat;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
